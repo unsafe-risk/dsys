91dacb85fbf6479e4c3087266f2bf2c004927110
lemon-mint <ice.lemons.mint@gmail.com>
2022-07-24 20:36:31 +0900

    protocol.go

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
+func main() {
+	ln, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+	defer ln.Close()
+	port := ln.Addr().(*net.TCPAddr).Port
+	fmt.Println("Listening on port", port)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	<-sig
 }
