3d29f97895203ea0836935f61a85945106bab786
lemon-mint <ice.lemons.mint@gmail.com>
2022-10-31 17:39:03 +0900

    LastApplied

--- consensus/raft/statemachine.go
+++ consensus/raft/statemachine.go
@@ -8,9 +8,12 @@ type StateMachine interface {
 	// Apply applies a command to the state machine.
 	Apply(command *raftproto.Entry) error
 
-	// Snapshot returns a snapshot of the state machine.
+	// Snapshot returns a snapshot stream of the state machine.
 	Snapshot() (<-chan *raftproto.Snapshot, error)
 
-	// Restore restores the state machine from a snapshot.
+	// Restore restores the state machine from a snapshot stream.
 	Restore(<-chan *raftproto.Snapshot) error
+
+	// LastApplied returns the index of the last applied command.
+	LastApplied() uint64
 }
